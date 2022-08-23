488d07458b12baa94682f7549dfcc7e2f1be2c50
Nicolas Santillan <panuelo81@outlook.com.ar>
2022-08-23 09:58:13 -0300

    add (fend) main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, NgControl, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  public form: FormGroup;
+
+  EmployeeArray: Employee[] = [
+    {id: 1, name: "Juan", country: "USA"},
+    {id: 2, name: "Tomy", country: "USA"},
+    {id: 3, name: "Fede", country: "USA"},
+  ];  
+
+  
+  
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void{
+    const ParaLosFormulariosTipo1  = {
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
+      country: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
+    }
+    this.form = this.formBuilder.group(ParaLosFormulariosTipo1);
+  }
+
+  ngDoCheck(){
+
+  }
+  
+  send(): any {
+    let EmployeeAdd: Employee = {
+      id : 0,
+      name : '',
+      country : ''
+    };  
+
+    EmployeeAdd.id = this.EmployeeArray.length + 1;
+    EmployeeAdd.name = this.form.value.name;
+    EmployeeAdd.country = this.form.value.country;
+    //spread
+    this.EmployeeArray = [EmployeeAdd, ...this.EmployeeArray];
+
+    this.EmployeeArray.sort( (a, b)=> b.id - a.id);
+    this.EmployeeArray = this.EmployeeArray.filter((employee)=> employee.name != 'hola');
+
+
+
+    console.log(this.EmployeeArray);
+  }
 }
